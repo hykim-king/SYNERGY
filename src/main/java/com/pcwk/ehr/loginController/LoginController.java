cf70188294eaec98052488218fa9e9fdaa53fc6b
이종민 <50688210+limitlesscollaboration@users.noreply.github.com>
2025-06-26 19:40:49 +0900

    Add files via upload

---
+++ src/main/java/com/pcwk/ehr/loginController/LoginController.java
@@ -0,0 +1,75 @@
+package com.pcwk.ehr.loginController;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pcwk.ehr.cmn.PLog;
+import com.pcwk.ehr.member.MemberDTO;
+import com.pcwk.ehr.member.service.MemberService;
+
+@Controller
+@RequestMapping("/member")
+public class LoginController implements PLog {
+
+	@Autowired
+	private MemberService memberService;
+
+	public LoginController() {
+
+	}
+
+	@GetMapping("/loginView.do")
+	public String loginView() {
+		return "member/login";
+	}
+
+	@PostMapping("/login.do")
+	public String login(String id, String pwd, HttpSession session, Model model) {
+
+		MemberDTO param = new MemberDTO();
+		param.setId(id);
+		
+		MemberDTO user = null;
+
+		try {
+	        user = memberService.doSelectOne(param); // DB에서 사용자 정보 조회
+	    } catch (Exception e) {
+	        model.addAttribute("message", "서버 오류가 발생했습니다.");
+	        return "member/login";
+	    }
+
+	    if (user == null) {
+	        model.addAttribute("message", "존재하지 않는 ID입니다.");
+	        return "member/login";
+	    }
+
+	    if (!user.getPwd().equals(pwd)) {
+	        model.addAttribute("message", "비밀번호가 일치하지 않습니다.");
+	        return "member/login";
+	    }
+	    //사용자가 로그인에 성공했을 때, 해당 사용자 정보를 세션에 "loginUser"라는 이름으로 저장
+	    session.setAttribute("loginUser", user);
+	    
+		if (user.getAdminRole() == 1) {
+			//redirect:/admin/main.do -> /admin/main.do로 이동
+	        return "redirect:/admin/main.do";
+	    } else {
+	        return "redirect:/member/main.do";
+	    }
+}
+
+	// 로그아웃
+	@GetMapping("/logout.do")
+	public String logout(HttpSession session) {
+		// 로그아웃하는 명령어
+		session.invalidate();
+		return "redirect:/member/loginView.do";
+	}
+
+}
