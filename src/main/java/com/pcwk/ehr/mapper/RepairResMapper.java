b842beac564b8537e6a41a2d592bf1acef289363
pcwk <syl6627@gmail.com>
2025-06-27 15:27:28 +0900

    yuijtyu

--- src/main/java/com/pcwk/ehr/mapper/RepairResMapper.java
+++
@@ -1,9 +0,0 @@
-package com.pcwk.ehr.mapper;
-
-import com.pcwk.ehr.cmn.WorkDiv;
-import com.pcwk.ehr.repair.RepairResDTO;
-
-
-public interface RepairResMapper extends WorkDiv<RepairResDTO> {
-
-}
---
+++ src/main/java/com/pcwk/ehr/mapper/Retailermapper.java
@@ -0,0 +1,22 @@
+package com.pcwk.ehr.mapper;
+
+import com.pcwk.ehr.cmn.WorkDiv;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.pcwk.ehr.cmn.WorkDiv;
+import com.pcwk.ehr.retailer.RetailerDTO;
+
+
+@Mapper
+public interface Retailermapper {
+    List<RetailerDTO> getAll();
+    RetailerDTO getOne(int retailer_code);
+
+    List<RetailerDTO> doRetrieveByCarMfList(@Param("carMfList") List<String> carMfList);
+
+    int getCount();
+}
