1e97df86077d2bacd98a60497e6d5f0225e60392
gyeong ryung <zoqwkdrudfud@gmail.com>
2025-07-01 13:42:25 +0900

    ddd

--- src/main/java/com/pcwk/ehr/board/BoardService.java
+++ src/main/java/com/pcwk/ehr/board/BoardService.java
@@ -18,27 +18,4 @@ public interface BoardService {
 	int getCount();
 
 	void deleteAll();
-
-	List<BoardDTO> doRetrieve(BoardMapperDTO searchVO);
-}
-
-package com.pcwk.ehr.board;
-
-import java.util.List;
-
-public interface BoardService {
-
-	int doSave(BoardDTO dto);
-
-	int doUpdate(BoardDTO dto);
-
-	int doDelete(BoardDTO dto);
-
-	BoardDTO doSelectOne(BoardDTO dto);
-
-	List<BoardDTO> doRetrieve(BoardDTO dto);
-
-	int getCount();
-
-	void deleteAll();
 }
