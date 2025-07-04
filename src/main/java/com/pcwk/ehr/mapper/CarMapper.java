3007e924762c0d855b8937574477a64b9e8e99c1
pcwk <syl6627@gmail.com>
2025-07-04 11:41:25 +0900

    Car update 추가 최종본

--- src/main/java/com/pcwk/ehr/mapper/CarMapper.java
+++ src/main/java/com/pcwk/ehr/mapper/CarMapper.java
@@ -5,33 +5,40 @@ import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import com.pcwk.ehr.car.CarDTO;
+import com.pcwk.ehr.cmn.DTO;
 
 @Mapper
 public interface CarMapper {
 
-    int getCarCode();
+	int getCarCode();
 
-    int getCarCount();
+	int getCarCount();
 
-    int deleteAll();
+	int deleteAll();
 
-    CarDTO getOne(int carCode);
+	CarDTO getOne(int carCode);
 
-    List<CarDTO> getCarsByBrand(@Param("brand") String brand);
+	CarDTO doSelectOne(CarDTO carDTO);
 
-    List<CarDTO> searchCars(@Param("keyword") String keyword);
+	List<CarDTO> getCarsByBrand(@Param("brand") String brand);
 
-    List<CarDTO> getAllCars();
+	List<CarDTO> searchCars(@Param("keyword") String keyword);
 
-    void insertCar(List<CarDTO> cars);
+	List<CarDTO> getAllCars();
 
-    int doSave(CarDTO car);
+	void insertCar(List<CarDTO> cars);
 
-    int doUpdate(CarDTO car);
+	int doSave(CarDTO car);
 
-    int doDeleteById(@Param("carCode") int carCode);
+	int doUpdate(CarDTO car);
 
-    int doDeleteAll();
+	int doDeleteById(@Param("carCode") int carCode);
 
-    List<CarDTO> getCarsByPage(Map<String, Integer> params);
+	int doDeleteAll();
+	
+	int doDelete(CarDTO car);
+
+	List<CarDTO> getCarsByPage(Map<String, Integer> params);
+
+	List<CarDTO> doRetrieve(DTO param);
 }
