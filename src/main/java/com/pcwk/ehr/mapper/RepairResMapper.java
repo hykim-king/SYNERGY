eea5993799b22fcd7ff341f958fc4b8b32d9c2b9
min <kamgyong@gmail.com>
2025-07-09 14:11:46 +0900

    컴밋

--- src/main/java/com/pcwk/ehr/mapper/RepairResMapper.java
+++ src/main/java/com/pcwk/ehr/mapper/RepairResMapper.java
@@ -2,13 +2,11 @@ package com.pcwk.ehr.mapper;
 
 
 import java.util.List;
-import java.util.Map;
 
 import org.apache.ibatis.annotations.Param;
 
 import com.pcwk.ehr.car.CarDTO;
 import com.pcwk.ehr.cmn.WorkDiv;
-import com.pcwk.ehr.drive.DriveResDTO;
 import com.pcwk.ehr.repair.RepairResDTO;
 import com.pcwk.ehr.retailer.RetailerDTO;
 
